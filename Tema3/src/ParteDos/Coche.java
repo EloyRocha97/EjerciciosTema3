b8f46e00efe4c032d48acecd02ae07559e5b59d5
Eloy Rocha <rochaeloy@gmail.com>
2022-04-26 21:45:54 -0300

    primer commit

---
+++ Tema3/src/ParteDos/Coche.java
@@ -0,0 +1,21 @@
+package ParteDos;
+
+public class Coche {
+    public static Integer puertas;
+
+    public static void incrementar(){
+        while(puertas <= 3){
+            puertas ++;
+        }
+    }
+
+    public static void main(String[] args){
+        Coche miCoche = new Coche();
+
+        miCoche.puertas = 1;
+
+        incrementar();
+
+        System.out.println("Esta es la cantidad de puerta/s del auto: " + puertas);
+    }
+}
